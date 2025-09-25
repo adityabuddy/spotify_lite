416b8f2b5c23eca134090468467658398ee0117e
adityabuddy <134672069+adityabuddy@users.noreply.github.com>
2025-09-25 23:18:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Define the database URL. This can be stored in environment variables for security.
+#  we are connecting to a MySQL database using the mysqlconnector driver.
 # For a local MySQL setup, the URL format is:
 # "mysql+mysqlconnector://<user>:<password>@<host>:<port>/<database>"
 DATABASE_URL = "mysql+mysqlconnector://spotifyuser:Aditya12@localhost:3306/spotify_lite"
@@ -11,10 +11,10 @@ DATABASE_URL = "mysql+mysqlconnector://spotifyuser:Aditya12@localhost:3306/spoti
 # Create a SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
-# Create a SessionLocal class. Each instance of this class will be a database session.
+# Create a SessionLocal class. 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Create a DeclarativeBase class. This is the base class for all our database models.
+# Create a DeclarativeBase class.
 # Think of it as the foundation upon which all our table definitions will be built.
 Base = declarative_base()
 
